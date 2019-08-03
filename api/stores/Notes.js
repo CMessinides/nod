39af78ca6077ec16143d09530712c3fad27682fc
Cameron Messinides <cameron.messinides@gmail.com>
2019-08-03 14:41:41 -0400

    feat(notes): attach note content when querying notes

--- api/stores/Notes.js
+++ api/stores/Notes.js
@@ -2,22 +2,63 @@ import db from "../db";
 import { snakeToCamel } from "../db/transformers";
 import { pipe } from "lodash/fp";
 import { makeSluggable } from "../../lib/slugs";
+import { pick } from "../../lib/utils";
 
 const prepareNote = pipe(
   snakeToCamel,
   makeSluggable
 );
 
+const prepareContentChunk = pipe(
+  snakeToCamel,
+  formatChunkByType
+);
+
+const SHARED_CHUNK_KEYS = ["id", "type", "prevChunkId"];
+function formatChunkByType(chunk) {
+  switch (chunk.type) {
+    case "task_list":
+      return pick(chunk, "name", ...SHARED_CHUNK_KEYS);
+    case "text_content":
+      return pick(chunk, "text", ...SHARED_CHUNK_KEYS);
+    default:
+      throw new Error("Unrecognized content chunk type: " + chunk.type);
+  }
+}
+
+async function getContent(note) {
+  const { rows } = await db.query(
+    "SELECT note_chunks.id, prev_chunk_id, type, name, text FROM note_chunks FULL JOIN note_text_content_chunks ON note_chunks.id = note_text_content_chunks.id FULL JOIN note_task_list_chunks ON note_chunks.id = note_task_list_chunks.id WHERE note_id = $1",
+    [note.id]
+  );
+
+  note.content = rows.map(prepareContentChunk);
+  return note;
+}
+
 module.exports = {
   async getById(id) {
-    const { rows } = await db.query("SELECT * FROM notes WHERE id = $1", [id]);
-    return rows[0] ? prepareNote(rows[0]) : null;
+    const { rows, rowCount } = await db.query(
+      "SELECT * FROM notes WHERE id = $1",
+      [id]
+    );
+
+    if (rowCount === 0) return null;
+
+    const note = rows[0];
+    await getContent(note);
+    return prepareNote(note);
   },
   async getByNotebookId(id) {
-    const { rows } = await db.query(
+    const { rows: notes } = await db.query(
       "SELECT * FROM notes WHERE notebook_id = $1",
       [id]
     );
-    return rows.map(prepareNote);
+
+    for (const note of notes) {
+      await getContent(note);
+    }
+
+    return notes.map(prepareNote);
   }
 };
--- api/stores/Notes.test.js
+++ api/stores/Notes.test.js
@@ -20,18 +20,69 @@ const MOCK_NOTES = [
   }
 ];
 
-it("should get note by ID", () => {
-  const note = MOCK_NOTES[0];
-  db.query.mockImplementationOnce(() => Promise.resolve({ rows: [note] }));
+const MOCK_CONTENT = [
+  {
+    id: 1,
+    note_id: 1,
+    type: "text_content",
+    prev_chunk_id: null,
+    name: null,
+    text: "lorem ipsum"
+  },
+  {
+    id: 2,
+    note_id: 1,
+    type: "task_list",
+    prev_chunk_id: 1,
+    name: "Chores",
+    text: null
+  }
+];
 
-  expect(Notes.getById(note.id)).resolves.toStrictEqual(snakeToCamel(note));
-  expect(db.query).lastCalledWith("SELECT * FROM notes WHERE id = $1", [
-    note.id
+it("should get note by ID", async () => {
+  expect.assertions(3);
+
+  const noteInDB = MOCK_NOTES[0];
+  db.query.mockClear();
+  db.query
+    .mockResolvedValueOnce({ rows: [noteInDB], rowCount: 1 })
+    .mockResolvedValueOnce({
+      rows: MOCK_CONTENT,
+      rowCount: MOCK_CONTENT.length
+    });
+
+  const note = await Notes.getById(noteInDB.id);
+
+  expect(note).toStrictEqual({
+    ...snakeToCamel(noteInDB),
+    content: [
+      {
+        id: 1,
+        type: "text_content",
+        prevChunkId: null,
+        text: "lorem ipsum"
+      },
+      {
+        id: 2,
+        type: "task_list",
+        prevChunkId: 1,
+        name: "Chores"
+      }
+    ]
+  });
+
+  expect(db.query).nthCalledWith(1, "SELECT * FROM notes WHERE id = $1", [
+    noteInDB.id
   ]);
+  expect(db.query).nthCalledWith(
+    2,
+    "SELECT note_chunks.id, prev_chunk_id, type, name, text FROM note_chunks FULL JOIN note_text_content_chunks ON note_chunks.id = note_text_content_chunks.id FULL JOIN note_task_list_chunks ON note_chunks.id = note_task_list_chunks.id WHERE note_id = $1",
+    [note.id]
+  );
 });
 
 it("should get return null if no note exists", () => {
-  db.query.mockImplementationOnce(() => Promise.resolve({ rows: [] }));
+  db.query.mockResolvedValueOnce({ rows: [], rowCount: 0 });
 
   expect(Notes.getById(1)).resolves.toBe(null);
 });
@@ -39,12 +90,17 @@ it("should get return null if no note exists", () => {
 it("should get notes by notebook ID", () => {
   const notebookId = 1;
   const notes = MOCK_NOTES.filter(note => note.notebook_id === notebookId);
-  db.query.mockImplementationOnce(() => Promise.resolve({ rows: notes }));
+  db.query
+    .mockResolvedValue({ rows: [], rowCount: 0 })
+    .mockResolvedValueOnce({ rows: notes, rowCount: notes.length });
 
   expect(Notes.getByNotebookId(notebookId)).resolves.toStrictEqual(
-    notes.map(snakeToCamel)
+    notes.map(note => ({
+      ...snakeToCamel(note),
+      content: []
+    }))
   );
-  expect(db.query).lastCalledWith(
+  expect(db.query).toHaveBeenCalledWith(
     "SELECT * FROM notes WHERE notebook_id = $1",
     [notebookId]
   );
