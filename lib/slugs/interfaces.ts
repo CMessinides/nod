0eb5d30c5d6971c21221c131d3726e7c38ce8b81
Cameron Messinides <cameron.messinides@gmail.com>
2019-08-06 14:43:18 -0400

    refactor(slugs): adjust type checks in sluggable decorator

--- lib/slugs/interfaces.ts
+++ lib/slugs/interfaces.ts
@@ -1,8 +1,5 @@
 export interface Sluggable {
 	id: number;
 	title: string;
-}
-
-export interface HasSlug {
 	slug: string;
 }
--- lib/slugs/sluggable.ts
+++ lib/slugs/sluggable.ts
@@ -1,11 +1,11 @@
 import slugify from "slugify";
-import { Sluggable, HasSlug } from "./interfaces";
+import { Sluggable } from "./interfaces";
 
 export default function sluggable(
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	SluggableClass: new (...args: any[]) => Sluggable
 ) {
-	return class extends SluggableClass implements HasSlug {
+	return class extends SluggableClass implements Sluggable {
 		public get slug() {
 			return slugify(this.title, { lower: true }) + "--" + this.id;
 		}
